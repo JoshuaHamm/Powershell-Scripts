c988b6b16ff069ee448726e88118a19efbf653ba
Josh Hamm <hamm.joshua.t@gmail.com>
2024-08-27 17:03:51 -0400

    removed naming

--- Distribution Group Export.ps1
+++ Distribution Group Export.ps1
@@ -3,6 +3,6 @@ Grab all members of a dynamic distribution group and export to CSV to verify val
 Change the group name to match the group you want to export
 Change the export path to match your desired location
 #>
-$Everybody = Get-DynamicDistributionGroup "zzzEveryone"
+$Everybody = Get-DynamicDistributionGroup "YOURGROUPHERE"
 $members = Get-Recipient -RecipientPreviewFilter $Everybody.RecipientFilter -OrganizationalUnit $Everybody.RecipientContainer 
 $members | export-csv c:\nexigen\everyone.csv
