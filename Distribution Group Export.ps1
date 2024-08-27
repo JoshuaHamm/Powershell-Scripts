0bbfc831ad06f48e19f9ccfb99961215971b5706
Josh Hamm <hamm.joshua.t@gmail.com>
2024-08-27 16:49:37 -0400

    Initial upload, validation testing for Dynamic Group setup

---
+++ Distribution Group Export.ps1
@@ -0,0 +1,8 @@
+<#
+Grab all members of a dynamic distribution group and export to CSV to verify validation of the group
+Change the group name to match the group you want to export
+Change the export path to match your desired location
+#>
+$Everybody = Get-DynamicDistributionGroup "zzzEveryone"
+$members = Get-Recipient -RecipientPreviewFilter $Everybody.RecipientFilter -OrganizationalUnit $Everybody.RecipientContainer 
+$members | export-csv c:\nexigen\everyone.csv
